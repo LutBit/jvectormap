a2521161fcbbdb143826ac39540b1aaa54103552
Lutz Bittner FRD OPYA <lutz.bittner@zf.com>
2022-12-21 16:27:15 +0100

    World travel map created and colors per continents updated

---
+++ countries.js
@@ -0,0 +1,19 @@
+var countriesVisited = {
+  "Africa": ["MA", "MU"],
+  "Asia": [ "IN", "SG", "ID", "HK"],
+  "Europe": [
+    "IE", "GB", "PT", "ES", "FR", "NL", "BE", "DE", "CH", "IT", "AT", "RO","MC",
+     "CZ", "HR", "GR", "TR", "HU", "SE", "SI", "IL", "PS", "AL","BA", "LI", "LV", "ME", "LU"
+  ],
+  "North America": ["US", "MX", "CA"],
+  "Oceania": ["AU"],
+};
+var continentColors = {
+  "Europe": "#c10000",
+  "North America": "#00cc00",
+  "South America": "#008000",
+  "Asia": "#f33e01",
+  "Africa": "#fed52e",
+  "Antarctica": "#0040ff",
+  "Oceania": "#c04080",
+}
